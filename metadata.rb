13d81bc603c069adf0170ca9d11b98492939fdcc
René Oelke <rene.oelke@gmail.com>
2012-11-14 12:01:37 +0100

    Added apt cookbook as dependency.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license           "Apache 2.0"
 description       "Installs and maintains php and php modules"
 version           "1.1.0"
 
+depends "apt"
 depends "build-essential"
 depends "xml"
 depends "mysql"
